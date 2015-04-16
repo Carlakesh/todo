33bb1b1aec2770f6dd1249ba332a71e96b4e44c6
Carla Kechichian <carla.kechichian@hotmail.com>
2015-04-16 12:23:18 -0700

    video 5

--- includes/connect.php
+++ includes/connect.php
@@ -4,7 +4,7 @@ $mysqli = new mysqli('localhost', 'root', 'root', 'tasks');
 //$mysqli->connect_error then we want it to die and have this message
 if ($mysqli->connect_error) {
 	die('Connect Error (' . $mysqli->connect_errno .')'
-		. $mysqli->connect_errnor);
+		. $mysqli->connect_error);
 }
 else {
 	echo "Connection made";
